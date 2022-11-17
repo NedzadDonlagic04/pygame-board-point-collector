cd2204b5cc985635278413a7279ec7705b623b43
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-11-17 14:21:49 +0100

    New web build

---
+++ classes.py
@@ -0,0 +1,116 @@
+import pygame
+import random
+
+class MyClock:
+    def __init__(self, fps):
+        self.CLOCK = pygame.time.Clock()
+        self.FPS = fps
+
+    def tick(self):
+        self.CLOCK.tick(self.FPS)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, rectWidth, rectHeight, screenWidth, screenHeight, color=[0, 0, 0]):
+        super().__init__()
+        self.image = pygame.Surface((rectWidth, rectHeight), pygame.SRCALPHA)
+        pygame.draw.rect(self.image, color, (0, 0, rectWidth, rectHeight), border_radius=10)
+        self.rect = self.image.get_rect( center = (screenWidth / 2, screenHeight - 50) )
+
+        self.SCREEN_WIDTH = screenWidth
+        self.SPEED = 5
+    
+    def update(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.left -= self.SPEED
+
+            if self.rect.left < 0:
+                self.rect.left = 0
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.right += self.SPEED
+
+            if self.rect.right > self.SCREEN_WIDTH:
+                self.rect.right = self.SCREEN_WIDTH
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, rectWidth, rectHeight, screenWidth, screenHeight, currentScore=0, color=[211, 211, 211]):
+        super().__init__()
+
+        x = random.randrange(rectWidth, screenWidth - rectWidth)
+
+        self.image = pygame.Surface((rectWidth, rectHeight), pygame.SRCALPHA)
+        pygame.draw.rect(self.image, color, (0, 0, rectWidth, rectHeight), border_radius=4)
+        self.rect = self.image.get_rect( bottomleft = (x, 0) )
+
+        if currentScore > 500:
+            self.score = random.randrange(-5, 60, 5)
+        else:
+            self.score = random.randrange(5, 50, 5)
+
+        if self.score < 5:
+            self.score = '/'
+        elif self.score > 50:
+            self.score = '*'
+
+        font = pygame.font.Font('./fonts/Pixeltype.ttf', 40)
+        text = font.render(str(self.score), False, 'black')
+        textRect = text.get_rect( center = (rectWidth / 2, rectHeight / 2))
+
+        self.image.blit(text, textRect)
+
+        self.SCREEN_HEIGHT = screenHeight
+        self.SPEED = 5
+    
+    def getScore(self):
+        return self.score
+
+    def update(self, score):
+        self.rect.bottom += self.SPEED
+
+        if self.rect.bottom > self.SCREEN_HEIGHT:
+            score.reduceScore(self.getScore()) 
+            self.kill()
+
+class Score:
+    def __init__(self, default=50):
+        self.score = default
+        self.makeScore()
+
+        self.GAME_OVER = False
+    
+    def makeScore(self):
+        font = pygame.font.Font('./fonts/Pixeltype.ttf', 40)
+        self.text = font.render('Score: ' + str(self.score), False, 'black')
+        self.textRect = self.text.get_rect( topleft = (20, 20))
+
+        if self.score < 0:
+            self.GAME_OVER = True
+    
+    def addScore(self, score):
+        if score == '/':
+            self.score //= 2
+        elif score == '*':
+            self.score *= 2
+        else:
+            self.score += score
+
+        self.makeScore()
+
+    def reduceScore(self, score):
+        if score == '*':
+            self.score //= 2
+        elif score != '/':
+            self.score -= score
+
+        self.makeScore()
+
+    def update(self, player, blocks):
+        blocksHit= pygame.sprite.spritecollide(player, blocks, False)
+
+        for block in blocksHit:
+            self.addScore(block.getScore())
+            block.kill()
+
+    def draw(self, screen):
+        screen.blit(self.text, self.textRect)
---
+++ main.py
@@ -0,0 +1,61 @@
+import pygame
+from sys import exit
+from classes import *
+import asyncio
+
+class Game:
+    def __init__(self, width, height, title):
+        pygame.init()
+
+        self.WIDTH = width
+        self.HEIGHT = height
+        self.SCREEN = pygame.display.set_mode((width, height))
+        self.SCREEN_BG = 'white'
+        
+        pygame.display.set_caption(title)
+
+        self.CLOCK = MyClock(60)
+
+        self.PLAYER = pygame.sprite.GroupSingle( Player(100, 20, width, height) )
+
+        self.BLOCKS = pygame.sprite.Group( Block(80, 50, width, height) )
+
+        self.SCORE = Score(50)
+    
+    def quit(self):
+        pygame.quit()
+        exit()
+    
+    async def run(self):
+        while True:
+            await asyncio.sleep(0)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.quit()
+            
+            self.SCREEN.fill(self.SCREEN_BG)
+
+            self.PLAYER.update()
+            self.PLAYER.draw(self.SCREEN)
+
+            self.BLOCKS.update(self.SCORE)
+            self.BLOCKS.draw(self.SCREEN)
+
+            self.SCORE.update(self.PLAYER.sprite, self.BLOCKS)
+            self.SCORE.draw(self.SCREEN)
+
+            if self.SCORE.GAME_OVER:
+                self.quit()
+
+            for block in self.BLOCKS:
+                if block.rect.bottom == self.HEIGHT / 3:
+                    self.BLOCKS.add(Block(80, 50, self.WIDTH, self.HEIGHT, self.SCORE.score))
+                    break
+
+            pygame.display.update()
+            self.CLOCK.tick()
+
+if __name__ == '__main__':
+    game = Game(800, 600, 'Point Collector')
+    asyncio.run(game.run())
